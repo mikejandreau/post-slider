d23886f0f5316da64c95dc59e09513b4daf00388
Mike Jandreau <mikejandreau@gmail.com>
2017-12-22 13:07:49 -0500

    Removed jQuery wrapper and set automatic cycling to stop after an arrow is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,73 @@
-// Post Slider scripts
+// Post Slider
+// Version: 1.0
 
-jQuery(document).ready(function($) {
-    // Variables
-    var postslider = document.querySelector('.postslider');
-    var numberOfSlides = document.querySelectorAll('.postslider-inner .postslider-item').length;
-    var slide = 1;
-    var slides = document.querySelectorAll('.postslider-inner .postslider-item');
-    var timingSlider = 5000; // Slide timer in milliseconds, 5000 = 5 seconds
+// Variables
+var postslider = document.querySelector('.postslider');
+var numberOfSlides = document.querySelectorAll('.postslider-inner .postslider-item').length;
+var slide = 1;
+var slides = document.querySelectorAll('.postslider-inner .postslider-item');
+var timingSlider = 5000; // Slide timer in milliseconds, 5000 = 5 seconds
 
-    // Get previous slide
-    function prevSlide() {
-      var indexCurrent = slide - 1;
+// Get previous slide
+function prevSlide() {
+  var indexCurrent = slide - 1;
 
-      if (slide > 1) slide -= 1;
-      else slide = numberOfSlides;
+  if (slide > 1) slide -= 1;
+  else slide = numberOfSlides;
 
-      var indexNext = slide - 1;
+  var indexNext = slide - 1;
 
-      addRemoveSliderClasses(indexCurrent, indexNext);
+  addRemoveSliderClasses(indexCurrent, indexNext);
+}
+
+// Get next slide
+function nextSlide() {
+  var indexCurrent = slide - 1;
+
+  if (slide < numberOfSlides) slide += 1;
+  else slide = 1;
+
+  var indexNext = slide - 1;
+
+  addRemoveSliderClasses(indexCurrent, indexNext);
+}
+
+// Add/remove classes to the item of the post slider
+function addRemoveSliderClasses(indexCurrent, indexNext) {
+
+  slides[indexCurrent].classList.add('hiddenSlide');
+  slides[indexNext].classList.add('currentSlide');
+
+  var transitionElements = document.querySelectorAll('.hiddenSlide');
+
+  for (var i = 0; i < transitionElements.length; i++) {
+    if (transitionElements[i] != slides[indexCurrent]) {
+      transitionElements[i].classList.remove("hiddenSlide");
     }
+  }
 
-    // Get next slide
-    function nextSlide() {
-      var indexCurrent = slide - 1;
+  slides[indexCurrent].classList.remove('currentSlide');
+}
 
-      if (slide < numberOfSlides) slide += 1;
-      else slide = 1;
+// Stop switching slides automatically after arrow is clicked
+function resetInterval() {
+  clearInterval(timer);
+  timer = 0;
+}
 
-      var indexNext = slide - 1;
+// Listeners for prev/next arrows
+if (postslider != null) {
+  document.querySelector('.arrow-prev').addEventListener('click', function(e) {
+    prevSlide();
+    resetInterval();
+  });
 
-      addRemoveSliderClasses(indexCurrent, indexNext);
-    }
+  document.querySelector('.arrow-next').addEventListener('click', function(e) {
+    nextSlide();
+    resetInterval();
+  });
 
-    // Add/remove classes to the item of the post slider
-    function addRemoveSliderClasses(indexCurrent, indexNext) {
-
-      slides[indexCurrent].classList.add('hiddenSlide');
-      slides[indexNext].classList.add('currentSlide');
-
-      var transitionElements = document.querySelectorAll('.hiddenSlide');
-
-      for (var i = 0; i < transitionElements.length; i++) {
-        if (transitionElements[i] != slides[indexCurrent]) {
-          transitionElements[i].classList.remove("hiddenSlide");
-        }
-      }
-
-      slides[indexCurrent].classList.remove('currentSlide');
-    }
-
-    // Reset timer for automatic sliding
-    function resetInterval() {
-      clearInterval(timer);
-      timer = setInterval(function() {
-        nextSlide();
-      }, timingSlider);
-    }
-
-    // Listeners for prev/next arrows
-    if (postslider != null) {
-      document.querySelector('.arrow-prev').addEventListener('click', function(e) {
-        prevSlide();
-        resetInterval();
-      });
-
-      document.querySelector('.arrow-next').addEventListener('click', function(e) {
-        nextSlide();
-        resetInterval();
-      });
-
-      var timer = setInterval(function() {
-        nextSlide();
-      }, timingSlider);
-    }
-});
+  var timer = setInterval(function() {
+    nextSlide();
+  }, timingSlider);
+}
